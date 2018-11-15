74e9995f1660e19ba2b8532bfa162396fc23b15c
Matteo Baglini <matteo.baglini@gmail.com>
2018-11-15 15:38:49 -0800

    add empty production and test projcts

---
+++ src/AccessControl.Tests/CheckTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace AccessControl.Tests
+{
+    public class CheckTests
+    {
+        [Fact]
+        public void ItWorks()
+        {
+            Assert.Equal("test", "test");
+        }
+    }
+}
