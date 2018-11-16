e7e0da209b915de7d9668901908e774cda9f5e7d
Matteo Baglini <matteo.baglini@gmail.com>
2018-11-15 18:10:13 -0800

    empty InMemoryAccountRepository (all test read)

---
+++ src/AccessControl.App/InMemoryAccountRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AccessControl.App
+{
+    public class InMemoryAccountRepository : IAccountRepository
+    {
+        public Account Load(String id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/AccessControl.Tests/InMemoryAccountRepositoryTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AccessControl.App;
+
+namespace AccessControl.Tests
+{
+    public class InMemoryAccountRepositoryTests : AccountRepositoryContractTests
+    {
+        protected override IAccountRepository CreateWith(String id, String name)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IAccountRepository CreateWithout(String id, String name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
