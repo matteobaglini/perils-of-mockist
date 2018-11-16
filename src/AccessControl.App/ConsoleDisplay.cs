2c283c4fb0037651b2bdc2cbc308fb3b7362204b
Matteo Baglini <matteo.baglini@gmail.com>
2018-11-15 16:56:11 -0800

    console display show welcome message - green

---
+++ src/AccessControl.App/ConsoleDisplay.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace AccessControl.App
+{
+    public class ConsoleDisplay : IDisplay
+    {
+        private readonly TextWriter writer;
+
+        public ConsoleDisplay(TextWriter writer)
+        {
+            this.writer = writer;
+        }
+
+        public void ShowWelcomeMessage(String name)
+        {
+            writer.WriteLine("Welcome {0}!", name);
+        }
+
+        public void ShowUnauthorizedAccess(String name)
+        {
+        }
+
+        public void ShowUnknownAccount()
+        {
+        }
+    }
+}
---
+++ src/AccessControl.Tests/ConsoleDisplayTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using AccessControl.App;
+using Xunit;
+
+namespace AccessControl.Tests
+{
+    public class ConsoleDisplayTests
+    {
+       /*
+        * TEST LIST:
+        * X welcome message
+        * - unauth message
+        * - unknown message
+        */
+
+        [Fact]
+        public void WelcomeMessage()
+        {
+            var writer = new StringWriter();
+            var display = new ConsoleDisplay(writer);
+
+            display.ShowWelcomeMessage("john");
+
+            Assert.Equal("Welcome john!" + Environment.NewLine, writer.ToString());
+        }
+    }
+}
