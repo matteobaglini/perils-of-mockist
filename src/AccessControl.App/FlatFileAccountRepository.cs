072b895ca20686d1423edc2e929f92f94936bb9f
Matteo Baglini <matteo.baglini@gmail.com>
2018-11-15 16:34:43 -0800

    add FlatFileAccountRepository test list

---
+++ src/AccessControl.App/FlatFileAccountRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AccessControl.App
+{
+    public class FlatFileAccountRepository : IAccountRepository
+    {
+        public Account Load(String id)
+        {
+            return null;
+        }
+    }
+}
---
+++ src/AccessControl.Tests/FlatFileAccountRepositoryTests.cs
@@ -0,0 +1,12 @@
+﻿namespace AccessControl.Tests
+{
+    public class FlatFileAccountRepositoryTests
+    {
+        /*
+         * TEST LIST:
+         * - account found
+         * - account not found
+         * - missing file
+         */
+    }
+}
